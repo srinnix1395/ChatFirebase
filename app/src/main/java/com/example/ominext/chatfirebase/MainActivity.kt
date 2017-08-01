e8f7d560b2e3ca7edea14f8410bc35cd92150bac
Tuha <tuha@ominext.com>
2017-08-01 17:16:21 +0700

    first commit

---
+++ app/src/main/java/com/example/ominext/chatfirebase/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ominext.chatfirebase
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Ominext on 8/1/2017.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
