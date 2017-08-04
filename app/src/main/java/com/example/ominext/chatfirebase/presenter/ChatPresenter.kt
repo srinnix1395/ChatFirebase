be174de40de0a6699ceac5e40a5cbd122df892ed
Tuha <tuha@ominext.com>
2017-08-04 11:11:30 +0700

    current

--- app/src/main/java/com/example/ominext/chatfirebase/presenter/ChatPresenter.kt
+++ app/src/main/java/com/example/ominext/chatfirebase/presenter/ChatPresenter.kt
@@ -1,6 +1,7 @@
 package com.example.ominext.chatfirebase.presenter
 
 import android.os.Bundle
+import com.example.ominext.chatfirebase.ChatApplication
 import com.example.ominext.chatfirebase.model.User
 import com.example.ominext.plaidfork.ui.chat.ChatConstant
 import com.example.ominext.plaidfork.ui.chat.Message
@@ -8,7 +9,6 @@ import com.example.ominext.plaidfork.ui.chat.view.ChatFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import java.util.*
 
 /**
@@ -20,36 +20,41 @@ class ChatPresenter {
     val listMessage: ArrayList<Any> = ArrayList()
     var urlImage: String? = null
     var currentUser: FirebaseUser? = null
-    lateinit var db:DatabaseReference
-    lateinit var userFriend: User
+    var db:DatabaseReference? = null
+    var userFriend: User? = null
     lateinit var conversationKey: String
 
     fun addView(chatFragment: ChatFragment) {
         view = chatFragment
-        db = FirebaseDatabase.getInstance().reference
+        db = ChatApplication.app?.db
         currentUser = FirebaseAuth.getInstance().currentUser
     }
 
     fun onLoadMessage() {
-
+        db?.child(ChatConstant.CONVERSATIONS)?.child(conversationKey)
     }
 
     fun getData(arguments: Bundle) {
         userFriend = arguments.getParcelable(ChatConstant.USER)
-        if (currentUser?.uid?.compareTo(userFriend.uid!!)!! > 0) {
-            conversationKey = currentUser?.uid + userFriend.uid
+        if (currentUser?.uid?.compareTo(userFriend?.uid!!)!! > 0) {
+            conversationKey = currentUser?.uid + userFriend?.uid
         } else {
-            conversationKey = userFriend.uid + currentUser?.uid
+            conversationKey = userFriend?.uid + currentUser?.uid
         }
     }
 
     fun sendMessage(text: String, typeMessage: Int) {
-        if (typeMessage == 1) {
+        val message: Message = Message()
+        message.idSender = currentUser?.uid
+        message.idReceiver = userFriend?.uid
+        message.status = ChatConstant.PENDING
 
+        if (typeMessage == 1) {
+//            message.messageType = Type.LIKE
         }
     }
 
     private fun insertMessage(message: Message) {
-        db.child(ChatConstant.CONVERSATIONS).child(conversationKey)
+        db?.child(ChatConstant.CONVERSATIONS)?.child(conversationKey)?.push()?.setValue(message)
     }
 }
